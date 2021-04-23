fa629d6dc62ee1b73ba323c3236947960fe8e3b7
Garrison Tyson <garrison.r.tyson@gmail.com>
2021-04-23 11:58:37 -0700

    created ls.js

--- bash.js
+++ bash.js
@@ -1,11 +1,16 @@
+const pwd = require('./pwd.js');
+const ls = require('./ls.js')
 // Output a prompt
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
-    const cmd = data.toString().trim(); 
+    const cmd = data.toString().trim();
     if (cmd === 'pwd'){
-        process.stdout.write(process.cwd())
+        pwd();
+
+    } else if (cmd === 'ls') {
+        ls();
     } else {
         process.stdout.write('You typed: ' + cmd);
         process.stdout.write('\nprompt > ')
---
+++ pwd.js
@@ -0,0 +1,6 @@
+
+// function pwd() {process.stdout.write(process.cwd())
+//   process.stdout.write('\nprompt > ')}
+
+module.exports = function(){process.stdout.write(process.cwd())
+  process.stdout.write('\nprompt > ')};
